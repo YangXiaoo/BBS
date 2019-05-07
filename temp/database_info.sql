47e14af3406c392d7e41c325b5c5d6df1cd3a0ba
YangXiaoo <33798487+YangXiaoo@users.noreply.github.com>
2019-05-07 12:41:18 +0800

    Initial commit

---
+++ temp/database_info.sql
@@ -0,0 +1,20 @@
+-- 2019/5/1
+
+user 			-- 用户
+user_info 		-- 用户信息
+grade			-- 等级
+role 			-- 角色
+role_user 		-- 角色管理
+follow			-- 关注/粉丝
+
+category 		-- 帖子分类
+user_article 	-- 文章
+favorite		-- 文章收藏
+browse			-- 文章浏览记录
+upvote 			-- 文章点赞
+comment 		-- 文章评论
+
+guestbook		-- 留言
+
+-- admin
+web_article	-- 网站主页文章
